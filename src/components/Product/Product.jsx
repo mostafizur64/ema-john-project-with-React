d511af8c456f9d7a27be1e3af16a4190ca67b2d0
mostafizur64 <mostafizurm01@gmail.com>
2023-03-28 14:59:35 +0600

    save cart to local storage!

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,7 +4,7 @@ import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props);
+    // console.log(props);
     const { name, img, price, seller, rating } = props.product;
     const handleAddToCard = props.handleAddToCard;
 
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -11,11 +12,16 @@ const Shop = () => {
     const handleAddToCard =(product)=>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
     useEffect(()=>{
         fetch('products.json').then(res=>res.json()).then(data=>setProducts(data));
 
         
+    },[])
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
     },[])
 
 
